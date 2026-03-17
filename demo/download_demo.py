c8ef94197a6205420be46d7df3e4d94760682587
runner99 <2825201723@qq.com>
2026-03-17 22:29:02 +0800

    test

--- demo/download_demo.py
+++ demo/download_demo.py
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     #
     # download_allStock(period_list=period_list,start_time=start_time,end_time=end_time)
 
+
     download_history('002415.SZ','1d','20241120000000','20241126000000')
